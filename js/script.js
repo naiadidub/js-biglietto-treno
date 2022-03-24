602ffaaa0fa0d9e50555d9d4f35d6ac6d3c066e4
naiadidub <antonio.zaghi1405@gmail.com>
2022-03-24 17:52:30 +0100

    end

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let distanza = parseInt( prompt("km di distanza dalla destinazione"));
+console.log(distanza);
+let età = parseInt(prompt("inserire età"));
+console.log(età);
+
+if (isNaN(distanza)){
+    alert ('ricaricare pagina')
+} else if (isNaN(età)){
+    alert ('ricaricare pagina')
+} else {
+    console.log('avanza')
+}
+
+
+let costoViaggio = distanza * 0.21
+console.log(costoViaggio);
+
+if (età < 18){
+    costoViaggio = (costoViaggio - (costoViaggio * 0.2))
+    console.log (costoViaggio)
+} else if (età > 65){
+    costoViaggio = (costoViaggio - (costoViaggio * 0.4))
+    console.log (costoViaggio)
+} else {
+    console.log (costoViaggio)
+}
+
+let decimalcosto = costoViaggio.toFixed(2);
+
+document.getElementById("price").innerHTML = (decimalcosto)
